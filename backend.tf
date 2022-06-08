09dd304918c651ebb522b07baa6a3af440b4feca
rinad <52731778+rrriii-dot@users.noreply.github.com>
2022-06-08 22:55:38 +0200

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "backend-bucket-terraform"
+    bucket = "backend-bucket-terraform-slave-r"
     key    = "TerraformStatus/Status.tfstate"
     region = "us-east-1"
   }
